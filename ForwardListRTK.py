9ece414719425f0f8ced95eb4f145270fe7ff71e
Дом <artvid29@gmail.com>
2023-06-14 09:13:41 +0300

    окончательный рабочий вариант, теперь осталось подцепить наблюдение за появлением файла, и как только он появится произвести,запуск этого скрипта

---
+++ ForwardListRTK.py
@@ -0,0 +1,20 @@
+import pandas as pd # подключение модуля pandas
+df = pd.read_excel('forwardList.xlsx')
+NumbersList = list(map(str, df['№ телефона'].tolist()))
+ForwardsList = list(map(str, df['№ переадресации'].tolist()))
+LengthOfList = len(NumbersList)
+
+
+NumbersListOut = list(map(lambda x: '+7812' + x, NumbersList))
+ForwardsListOut = list(map(lambda x: '8107' + x, ForwardsList))
+
+print(NumbersListOut)
+print(ForwardsListOut)
+
+
+MyFile = open('переадпесация.txt', 'w')
+
+for k in range(LengthOfList-1):
+    MyFile.writelines(NumbersListOut[k] + ' ' + ForwardsListOut[k])
+
+MyFile.close()
